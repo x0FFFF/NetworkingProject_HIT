7425380c2da5871be5cdd4257ba567356de957ac
Daniel Shelgunov <48632072+x0FFFF@users.noreply.github.com>
2025-12-14 11:07:32 +0200

    feat: added server CLI

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 import socket
+import cmd
+
+class ServerCLI(cmd.Cmd):
+    prompt = 'Server> '
+    intro = "Starting up..."
+
+    def __init__(self):
+        super().__init__()
+
+    def do_quit(self, line) -> bool:
+        """Quit the server"""
+        print("Closing server...")
+        return True
+
 
 def main():
-    pass
+    ServerCLI().cmdloop()
 
 
 if __name__ == '__main__':
